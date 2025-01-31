cae878d9f8cb141f3635f19611a7d362dc135b22
Milen Nikolov <milenski1987@icloud.com>
2025-02-01 00:37:42 +0200

    small updates
    Added color to output.
    Added sleep time between different options

--- personal_fitness_tracker_system.py
+++ personal_fitness_tracker_system.py
@@ -1,3 +1,6 @@
+from time import sleep
+from colorama import Fore
+
 # Personal Fitness Tracker System 🏋️‍♂️
 
 
@@ -12,13 +15,13 @@ calorie_goal = 0  # Daily calorie intake goal
 def log_workout(workout_type, duration): #log workout type and duration
     workouts.append(workout_type)
     workouts.append(duration)
-    print(f"{duration} minutes of {workout_type} added successfully!\n")
+    return f"{duration} minutes of {workout_type} added successfully!\n"
 
 
 
 def log_calorie_intake(calories_consumed): #log calories intake for current meal
     calories.append(calories_consumed)
-    print(f"{calories_consumed} calories added!\n")
+    return f"{calories_consumed} calories added!\n"
 
 
 
@@ -28,15 +31,15 @@ def view_progress():
         total_workout_time += workouts[i]
     total_calories = sum(calories)
 
-    print(f"Total workout time for today: {total_workout_time} minutes.")
-    print(f"Total calories consumed today: {total_calories} calories.")
-    print("Good results. Keep going!\n")
+    return  f"Total workout time for today: {total_workout_time} minutes.\
+    \nTotal calories consumed today: {total_calories} calories.\
+    \nKeep going!\n"
 
 
 def reset_progress(): #progrss reset
     workouts.clear()
     calories.clear()
-    print(f"Your workout and calories logs are clear.")
+    return f"Your workout and calories logs are clear.\n"
 
 
 
@@ -46,7 +49,7 @@ def set_daily_goals(workout_minutes, calorie_limit):
 
     workout_goal = workout_minutes
     calorie_goal = calorie_limit
-    print(f"Workout and Calorie goals are set successfully!\n")
+    return f"Workout and Calories goals are set successfully!\n"
 
 
 
@@ -58,8 +61,8 @@ def encouragement_system():
 
     workout_percent = round(total_workout_time / workout_goal * 100)
     calories_percent = round(total_calories / calorie_goal * 100)
-    print(f"You reached {workout_percent}% of daily workout goal and {calories_percent}% of daily calories goal\n")
-    print("Push harder than yesterday if you want a different tomorrow.")
+    return f"You reached {workout_percent}% of daily workout goal and {calories_percent}% of daily calories goal\
+    \nPush harder than yesterday if you want a different tomorrow.\n"
 
 
 def main():
@@ -67,76 +70,88 @@ def main():
 
     while True:
         # Display menu options
-        print("1. Log Workout")
+        print(Fore.MAGENTA + "1. Log Workout")
         print("2. Log Calorie Intake")
         print("3. View Progress")
         print("4. Reset Progress")
         print("5. Set Daily Goals")
         print("6. Check daily progress compared to goals")
         print("7. Exit")
+        sleep(1)
 
         # Prompt user for their choice
         choice = input("\nEnter your choice: ")
 
 
         if choice == '1':
-            type_of_workout = input("Enter the type of workout:")
-            duration_of_workout = int(input("Enter duration of workout in minutes:"))
+            type_of_workout = input("Enter the type of workout: ")
+            sleep(0.5)
+            duration_of_workout = int(input("Enter duration of workout in minutes: "))
+            sleep(0.5)
             if duration_of_workout <= 0:
-                print("Invalid input. Program is restarting!")
+                print(Fore.RED + "Invalid input. Program is restarting!")
                 continue
-            log_workout(type_of_workout, duration_of_workout)
+            print(Fore.GREEN + log_workout(type_of_workout, duration_of_workout))
+            sleep(1)
 
 
         elif choice == '2':
             current_calories = int(input("Enter calories for current meal:"))
+            sleep(0.5)
             if current_calories <= 0:
-                print("Invalid input. Program is restarting!")
+                print(Fore.RED + "Invalid input. Program is restarting!")
                 continue
-            log_calorie_intake(current_calories)
+            print(Fore.GREEN + log_calorie_intake(current_calories))
+            sleep(1)
 
 
         elif choice == '3':
-            view_progress()
-
+            sleep(0.5)
+            print(Fore.GREEN + view_progress())
+            sleep(1)
 
         elif choice == '4':
             confirmation = input("Are you sure?:")
+            sleep(0.5)
             if confirmation.lower() == "yes":
-                reset_progress()
-
+                print(Fore.GREEN + reset_progress())
+                sleep(1)
 
         elif choice == '5':
             print("Please enter your daily goals:")
+            sleep(0.5)
             minutes = int(input("Workout minutes for today:"))
+            sleep(0.5)
             if minutes <= 0:
-                print("Invalid input. Program is restarting!")
+                print(Fore.RED + "Invalid input. Program is restarting!")
                 continue
             calorie = int(input("Calories to consume today:"))
-
+            sleep(0.5)
             if calorie <= 0:
-                print("Invalid input. Program is restarting!")
+                print(Fore.RED + "Invalid input. Program is restarting!")
                 continue
-            set_daily_goals(minutes, calorie)
-
+            print(Fore.GREEN + set_daily_goals(minutes, calorie))
+            sleep(1)
 
         elif choice == '6':
             if workout_goal == 0 or calorie_goal == 0:
-                print("Your daily goals are not set!")
+                print(Fore.RED + "Your daily goals are not set!")
             elif not workouts:
-                print("You don't have any workouts yet.")
+                print(Fore.RED + "You don't have any workouts yet.")
             elif not calories:
-                print("No meals consumed today.")
+                print(Fore.RED + "No meals consumed today.")
             else:
-                encouragement_system()
-
+                sleep(0.5)
+                print(Fore.GREEN + encouragement_system())
+                sleep(1)
 
         elif choice == '7':
-            print("Thank you for using the Fitness Tracker. Stay healthy! 💪")
+            sleep(0.5)
+            print(Fore.CYAN + "Thank you for using the Fitness Tracker. Stay healthy! 💪")
             break
 
         else:
-            print("Invalid choice, please try again.")
+            print(Fore.RED + "Invalid choice, please try again.")
 
 
 if __name__ == "__main__":
